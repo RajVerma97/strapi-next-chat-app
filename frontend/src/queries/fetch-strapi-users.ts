9259980921819c99751b33e0b44e36811f07ebbd
RajVerma97 <rajneeskumar2545@gmail.com>
2024-12-06 00:08:41 +0530

    chore:add the NEXT_PUBLIC_BAKEND in fetch users

--- frontend/src/queries/fetch-strapi-users.ts
+++ frontend/src/queries/fetch-strapi-users.ts
@@ -1,8 +1,11 @@
-import { NEXT_PUBLIC_STRAPI_API_TOKEN } from "@/contants/constants";
+import {
+  NEXT_PUBLIC_BACKEND,
+  NEXT_PUBLIC_STRAPI_API_TOKEN,
+} from "@/contants/constants";
 import axios from "axios";
 
 export default async function fetchStrapiUsers() {
-  const response = await axios.get("http://localhost:1337/api/users", {
+  const response = await axios.get(`${NEXT_PUBLIC_BACKEND}/api/users`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${NEXT_PUBLIC_STRAPI_API_TOKEN}`,
